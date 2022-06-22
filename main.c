79b1322a0be2c2e0ea850eaff26f1e730d805aa4
jacksontcarroll <jacksontcarroll@gmail.com>
2022-06-22 18:18:51 -0400

    Cleaned up code, slight Nord changes

--- main.c
+++ main.c
@@ -4,15 +4,21 @@
 #include <stdbool.h>
 #include <math.h>
 
-// Global variable used to define how big the window size should be
+// These can't be const because they can be passed into the program
 int WIDTH;
 int HEIGHT; 
-int ITERATIONS = 16;
-int INITIAL_ITERATIONS;
 
-// Color palette used to draw the set
-// The earlier in the set, the lighter the color should be
+// The number of iterations before determining if a number is part of the set
+int iterations = 50;
+int initial_iterations = 16;
+
+// Used to define the sizes of the color scheme arrays
 #define NUMCOLORS 8
+
+// Color palettes, based on OneDark, Nord, and Gruvbox themes
+// The first color is the base color, colors after that go in
+// order of how close they are to the main set
+// Formatted 0xRRGGBBAA
 
 const int PALETTE_ONEDARK[NUMCOLORS] = {
 	0x282c34ff,
@@ -28,7 +34,7 @@ const int PALETTE_ONEDARK[NUMCOLORS] = {
 const int PALETTE_NORD[NUMCOLORS] = {
 	0x2e3440ff,
 	0xe5e9f0ff,
-	0xa3be8cff,
+	0xd8dee9ff,
 	0x8fbcbbff,
 	0x88c0d0ff,
 	0x81a1c1ff,
@@ -47,6 +53,7 @@ const int PALETTE_GRUVBOX[NUMCOLORS] = {
 	0xd3869bff
 };
 
+// The currently active palette (defined in main()) 
 int palette[NUMCOLORS]; 
 
 typedef struct {
@@ -64,31 +71,22 @@ typedef struct {
 	double imag;
 } Complex_Number;
 
-// Boxes
-Box resizeWindow;
-Box currentWindow;
+Box resizeWindow;	// Automatically determined when you click somewhere on screen (measured in px)
+Box currentWindow;	// The current Cartesian coordinates of the bounds of the screen
 
-bool resizeActive = false;
+// Min and Max functions for doubles, the only thing we need it for
+double min(const double a, const double b) { return (a >= b) ? b : a; }
+double max(const double a, const double b) { return (a >= b) ? a : b; }
 
-// Min and Max functions
-double min(const double a, const double b) {
-	if (a >= b) return b;
-	return a;
-}
-
-double max(const double a, const double b) {
-	if (a >= b) return a;
-	return b;
-}
-
-// Figure out if a given cartesian coordinate pair is in the Mandelbrot set
+// Takes in a Cartesian coordinate pair cc and returns the
+// number of iterations before f(z) > 2
 int iterate(Coordinates* cc) {
 	Complex_Number z = {0, 0};
 	Complex_Number z_temp = z;
 	Complex_Number c = {cc->x, cc->y};
 
 	int i = 0;
-	while (i < ITERATIONS && (z.real * z.real) + (z.imag * z.imag) < 4) {
+	while (i < iterations && (z.real * z.real) + (z.imag * z.imag) < 4) {
 		z_temp = z;
 
 		z.real = ((z_temp.real * z_temp.real) - (z_temp.imag * z_temp.imag)) + c.real;
@@ -100,18 +98,18 @@ int iterate(Coordinates* cc) {
 	return i;
 }
 
-// Changes base to the smaller Cartesian plane coordinates
+// Change of base from pixels to Cartesian plane coordinates
 Coordinates downscaleFromPixels(const Coordinates* sc) {
-	Coordinates cc;
-
-	cc.x = currentWindow.top_left.x + (sc->x / (double) WIDTH) * (currentWindow.bottom_right.x - currentWindow.top_left.x);
-	cc.y = currentWindow.top_left.y + (sc->y / (double) HEIGHT) * (currentWindow.bottom_right.y - currentWindow.top_left.y);
+	Coordinates cc = {
+		currentWindow.top_left.x + (sc->x / (double) WIDTH) * (currentWindow.bottom_right.x - currentWindow.top_left.x),
+		currentWindow.top_left.y + (sc->y / (double) HEIGHT) * (currentWindow.bottom_right.y - currentWindow.top_left.y)
+	};
 	
 	return cc;
 }
 
-// Generate the Mandelbrot set into an SDL_Surface object
-void generateMandelbrotSet(SDL_Surface *pixels) {
+// Takes in an SDL_Surface, fills it with Mandelbrot set pixels
+void generateMandelbrotSet(SDL_Window *window, SDL_Surface *pixels, SDL_Surface *screen) {
 	int pitch = pixels->pitch;
 
 	// Write to the pixels
@@ -127,44 +125,48 @@ void generateMandelbrotSet(SDL_Surface *pixels) {
 
 			int result = iterate(&cc);
 
-			if (result == ITERATIONS) {
+			if (result == iterations) {
 				row[x] = palette[0];
 			}
 			else {
-				row[x] = palette[1 + (int)round((NUMCOLORS - 1) * result / (double)ITERATIONS)];
+				row[x] = palette[1 + (int)round((NUMCOLORS - 1) * result / (double)iterations)];
 			}
 		}
 	}
 
 	// Stop writing
 	SDL_UnlockSurface(pixels);
+
+	// Put the pixels on screen
+	SDL_BlitSurface(pixels, NULL, screen, NULL);
+	SDL_UpdateWindowSurface(window);
 }
 
-// Resize the window on second click
-void resize(SDL_Surface *pixels) {
+// Resize the window on click
+void resize(SDL_Window *window, SDL_Surface *pixels, SDL_Surface *screen) {
 	// Get the current window corresponding to the resize window that was drawn
 	currentWindow.top_left = downscaleFromPixels(&resizeWindow.top_left);
 	currentWindow.bottom_right = downscaleFromPixels(&resizeWindow.bottom_right);
 
 	// Finally, re-draw the set
-	generateMandelbrotSet(pixels);
+	generateMandelbrotSet(window, pixels, screen);
 }
 
+// Set the visible Cartesian window to the default values
 void resetCurrentWindow() {
-	// Initialize Window values
 	currentWindow.top_left.x = -2;
 	currentWindow.bottom_right.x = 1;
 	currentWindow.top_left.y = -1.5;
 	currentWindow.bottom_right.y = 1.5;
 }
 
+// Update the color palette by copying over all the color values to palette[]
 void setPalette(const int *arr) {
 	for (int i = 0; i < NUMCOLORS; i++, arr++) {
 		palette[i] = *arr;
 	}
 }
 
-// Main
 int main(int argc, char *argv[]) {
 	bool isRunning = true;
 	
@@ -173,37 +175,28 @@ int main(int argc, char *argv[]) {
 		SDL_LogError(SDL_LOG_PRIORITY_ERROR, "Usage: Mandelbrot.exe {WIDTH} {HEIGHT}");
 	}
 
-	if (argc == 1) {
-		WIDTH = 1000;
-		HEIGHT = 1000;
-	}
-	else {
-		WIDTH = atoi(argv[1]);
-		HEIGHT = atoi(argv[2]);
-	}
+	// Set the dimensions
+	const bool dimensionsPassedIn = (argc == 1);
+	WIDTH = (dimensionsPassedIn) ? 1000 : atoi(argv[1]);
+	HEIGHT = (dimensionsPassedIn) ? 1000 : atoi(argv[2]);
 
-	// Initialize
+	// Initialize SDL stuff
 	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window *window = SDL_CreateWindow("Mandelbrot Set Explorer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+	SDL_Window *window = SDL_CreateWindow("Mandelbrot Set Explorer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
 	SDL_Surface *screen = SDL_GetWindowSurface(window);
 	SDL_Surface *pixels = SDL_CreateRGBSurfaceWithFormat(0, WIDTH, HEIGHT, 32, SDL_PIXELFORMAT_RGBX8888);
 
-	// Initialize window and iterations and palette
+	// Initialize window and set the default color scheme
 	resetCurrentWindow();
-	INITIAL_ITERATIONS = ITERATIONS;
 	setPalette(PALETTE_ONEDARK);
 
-	// Set pixels to black
+	// Initialize all pixels to black
 	SDL_FillRect(pixels, NULL, 0);
 
-	// Actually generate the set image
-	generateMandelbrotSet(pixels);
+	// Actually generate the Mandelbrot Set
+	generateMandelbrotSet(window, pixels, screen);
 
-	// Put the pixels on screen
-	SDL_BlitSurface(pixels, NULL, screen, NULL);
-	SDL_UpdateWindowSurface(window);
-
-	// Run the program
+	// Event handling
 	SDL_Event ev;
 	while (isRunning) {
 		while (SDL_PollEvent(&ev) != 0) {
@@ -211,15 +204,13 @@ int main(int argc, char *argv[]) {
 			if (ev.type == SDL_QUIT) { isRunning = false; }
 
 			// Mouse events
-			else if (ev.type == SDL_MOUSEBUTTONDOWN) {
+			else if (ev.type == SDL_MOUSEBUTTONDOWN && ev.button.button == 1) {
 				resizeWindow.bottom_right.x = min(ev.button.x + WIDTH/3, WIDTH);
 				resizeWindow.bottom_right.y = min(ev.button.y + HEIGHT/3, HEIGHT);
 				resizeWindow.top_left.x = max(ev.button.x - WIDTH/3, 0);
 				resizeWindow.top_left.y = max(ev.button.y - HEIGHT/3, 0);
 
-				resize(pixels);
-				SDL_BlitSurface(pixels, NULL, screen, NULL);
-				SDL_UpdateWindowSurface(window);
+				resize(window, pixels, screen);
 			}
 
 			// Keyboard events
@@ -236,35 +227,30 @@ int main(int argc, char *argv[]) {
 						SDL_SaveBMP(pixels, "Mandelbrot.bmp");
 						break;
 					case SDLK_r:
+						// Reset the image size
 						resetCurrentWindow();
-						ITERATIONS = INITIAL_ITERATIONS;
-						generateMandelbrotSet(pixels);
-						SDL_BlitSurface(pixels, NULL, screen, NULL);
-						SDL_UpdateWindowSurface(window);
+						iterations = initial_iterations;
+						generateMandelbrotSet(window, pixels, screen);
 						break;
 					case SDLK_i:
-						ITERATIONS *= 2;
-						generateMandelbrotSet(pixels);
-						SDL_BlitSurface(pixels, NULL, screen, NULL);
-						SDL_UpdateWindowSurface(window);
+						// Increase iterations
+						iterations = round(1.5 * iterations);
+						generateMandelbrotSet(window, pixels, screen);
 						break;
 					case SDLK_1:
+						// Set palette to OneDark
 						setPalette(PALETTE_ONEDARK);
-						generateMandelbrotSet(pixels);
-						SDL_BlitSurface(pixels, NULL, screen, NULL);
-						SDL_UpdateWindowSurface(window);
+						generateMandelbrotSet(window, pixels, screen);
 						break;
 					case SDLK_2:
+						// Set palette to Nord
 						setPalette(PALETTE_NORD);
-						generateMandelbrotSet(pixels);
-						SDL_BlitSurface(pixels, NULL, screen, NULL);
-						SDL_UpdateWindowSurface(window);
+						generateMandelbrotSet(window, pixels, screen);
 						break;
 					case SDLK_3:
+						// Set palette to Gruvbox
 						setPalette(PALETTE_GRUVBOX);
-						generateMandelbrotSet(pixels);
-						SDL_BlitSurface(pixels, NULL, screen, NULL);
-						SDL_UpdateWindowSurface(window);
+						generateMandelbrotSet(window, pixels, screen);
 						break;
 				}
 			}
